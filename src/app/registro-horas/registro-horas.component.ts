30cea7996156d606d83f4623c5ba648f3c30cd53
Brandon Fuentes Micete <115325863+BrandonFnts@users.noreply.github.com>
2026-08-06 19:11:01 -0600

    Proyecto para aprender angular

---
+++ src/app/registro-horas/registro-horas.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registro-horas',
+  templateUrl: './registro-horas.component.html',
+  styleUrl: './registro-horas.component.scss',
+})
+export class RegistroHorasComponent {
+
+}
---
+++ src/app/reportes/corte-de-caja/corte-de-caja.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-corte-de-caja',
+  templateUrl: './corte-de-caja.component.html',
+  styleUrl: './corte-de-caja.component.scss',
+})
+export class CorteDeCajaComponent {
+
+}
